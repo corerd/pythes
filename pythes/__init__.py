6fd4ad97b80b8ec41cc827f011178b244a4a6652
Martin Hlavňa <martin.hlavna@4q.sk>
2024-07-22 15:12:49 +0200

    Upgraded project to package structure so it can be install using pip

---
+++ pythes/__init__.py
@@ -0,0 +1,1 @@
+from .pythes import PyThes
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyThes',
+    version='1.0.0',
+    url='https://github.com/corerd/pythes',
+    author='Corrado Ubezio',
+    author_email='author@example.com',
+    description='PyThes is a Python class providing methods to search Hunspell thesaurus for words and related '
+                'information on part of speech, meanings and synonyms.',
+    packages=['pythes'],
+    install_requires=[],
+)
